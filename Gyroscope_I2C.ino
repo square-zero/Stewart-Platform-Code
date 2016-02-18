85ebfc27a86f74b341a81791317088e1a927b1d9
square-zero <lkraft91@gmail.com>
2016-02-18 12:02:05 -0800

    Added files via upload

---
+++ Gyroscope_I2C.ino
@@ -0,0 +1,170 @@
+/*****************************************************************
+LSM9DS1_Basic_I2C.ino
+SFE_LSM9DS1 Library Simple Example Code - I2C Interface
+Jim Lindblom @ SparkFun Electronics
+
+
+Hardware setup: This library supports communicating with the
+LSM9DS1 over either I2C or SPI. This example demonstrates how
+to use I2C. The pin-out is as follows:
+  LSM9DS1 --------- Arduino
+   SCL ---------- SCL (A5 on older 'Duinos')
+   SDA ---------- SDA (A4 on older 'Duinos')
+   VDD ------------- 3.3V
+   GND ------------- GND
+(CSG, CSXM, SDOG, and SDOXM should all be pulled high. 
+Jumpers on the breakout board will do this for you.)
+
+The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
+off the 3.3V rail! I2C pins are open-drain, so you'll be 
+(mostly) safe connecting the LSM9DS1's SCL and SDA pins 
+directly to the Arduino.
+
+Development environment specifics:
+  IDE: Arduino 1.6.3
+  Hardware Platform: SparkFun Redboard
+  LSM9DS1 Breakout Version: 1.0
+
+Distributed as-is; no warranty is given.
+*****************************************************************/
+// The SFE_LSM9DS1 library requires both Wire and SPI be
+// included BEFORE including the 9DS1 library.
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+////////////////////////////
+// Sketch Output Settings //
+////////////////////////////
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+
+// Earth's magnetic field varies by location. Add or subtract 
+// a declination to get a more accurate heading. Calculate 
+// your's here:
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
+void setupGyro() 
+{
+  
+  Serial.begin(115200);
+  
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    while (1)
+      ;
+  }
+}
+
+void printGyro()
+{
+  // To read from the gyroscope, you must first call the
+  // readGyro() function. When this exits, it'll update the
+  // gx, gy, and gz variables with the most current data.
+  imu.readGyro();
+  
+  // Now we can use the gx, gy, and gz variables as we please.
+  // Either print them as raw ADC values, or calculated in DPS.
+  Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcGyro helper function to convert a raw ADC value to
+  // DPS. Give the function the value that you want to convert.
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
+}
+
+void printAccel()
+{
+  // To read from the accelerometer, you must first call the
+  // readAccel() function. When this exits, it'll update the
+  // ax, ay, and az variables with the most current data.
+  imu.readAccel();
+  
+  // Now we can use the ax, ay, and az variables as we please.
+  // Either print them as raw ADC values, or calculated in g's.
+  Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcAccel helper function to convert a raw ADC value to
+  // g's. Give the function the value that you want to convert.
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.println(" g");
+#elif defined PRINT_RAW 
+  Serial.print(imu.ax);
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+
+}
+
+void printMag()
+{
+  // To read from the magnetometer, you must first call the
+  // readMag() function. When this exits, it'll update the
+  // mx, my, and mz variables with the most current data.
+  imu.readMag();
+  
+  // Now we can use the mx, my, and mz variables as we please.
+  // Either print them as raw ADC values, or calculated in Gauss.
+  Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcMag helper function to convert a raw ADC value to
+  // Gauss. Give the function the value that you want to convert.
+  Serial.print(imu.calcMag(imu.mx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.my), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.mz), 2);
+  Serial.println(" gauss");
+#elif defined PRINT_RAW
+  Serial.print(imu.mx);
+  Serial.print(", ");
+  Serial.print(imu.my);
+  Serial.print(", ");
+  Serial.println(imu.mz);
+#endif
+}
+
