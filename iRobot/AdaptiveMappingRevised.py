ce5968d90a22cce87f927731d027c96345822d19
elchung <elchung@uw.edu>
2016-06-06 13:53:38 -0700

    Update AdaptiveMappingRevised.py
    added moving commands directly into file

--- iRobot/AdaptiveMappingRevised.py
+++ iRobot/AdaptiveMappingRevised.py
@@ -357,3 +357,80 @@ class AdaptiveMapping:
         except struct.error:
             print "Got unexpected data from serial port."
             return None
+            
+     def move_forward(self):
+        sleep_time = .001 #in seconds
+        acceleration = 1 #in mm/s/s
+        curr_speed = 0 #in mm/s
+        max_speed = 400 #in mm/s
+        # goal_distance = 15680 #raw serial number from mouse.ino for 14 inch movement
+        # deceleration_distance = 0.5 *  (((max_speed * sleep_time)**2) / acceleration)  
+            #14 incehs, maximize speed, max speed at 7 inches, 
+            #200 mm/s is max speed, accelerate slowly until reach max speed    
+    
+        for i in range(max_speed): #accelerate 1mm/s every ms
+            curr_speed += acceleration
+            # Ryan's edit to convert curr_speed to high and low bytes
+#             (high, low) = bytes(curr_speed)
+            (high, low) = divmod(curr_speed, 0x100)
+            self.sendCommandASCII('145 ' + str(high) + " " + str(low) + " " + str(high) + " " + str(low)) #
+            time.sleep(sleep_time)
+    
+        # accel_distance = ser.readline()    
+    
+        #now at max speed, check total distance
+        # while goal_distance - ser.readline() > accel_distance:
+        #      pass #do nothing, wait until within distance
+        time.sleep(0.12)
+    
+        #at this point goal_distance - mouse_distance <= deceleration_distance:
+        for i in range(max_speed):
+             #decelerate
+            curr_speed -= acceleration
+            (high, low) = divmod(curr_speed, 0x100)
+            self.sendCommandASCII('145 ' + str(high) + " " + str(low) + " " + str(high) + " " + str(low)) #
+            time.sleep(sleep_time)
+        
+        self.sendCommandASCII('137 00 00 00 00') #stop
+         #congrats, you are now stopped 
+
+    def turn_left(self):
+        #x_max = 50
+        
+        self.sendCommandASCII('137 255 56 255 255') #turn counterclockwise/left
+    
+        time.sleep(0.95) #test this to make sure that it truly turns 90 deg
+        # while ser.readline() < x_max:
+        #     time.sleep(1) #or just pass
+        self.sendCommandASCII('137 00 00 00 00') #stop
+    
+    
+    def turn_right(self):
+        # x_max = -50
+    
+        self.sendCommandASCII('137 255 56 00 01') #turn clockwise
+        
+        time.sleep(0.95)
+    
+        # while ser.readline() < x_max: #in case x and y values are individually slightly off...fucked if they ever are
+        #     pass
+        self.sendCommandASCII('137 00 00 00 00') #stop
+    
+    
+    def turn_around(self):
+        # x_max = 100 #(double turn left/right max)
+        
+        self.sendCommandASCII('137 255 56 255 255')
+    
+        time.sleep(1.9)
+        # while ser.readline() < x_max:
+        #     time.sleep(1)
+        self.sendCommandASCII('137 00 00 00 00')
+#     
+#     def dec_to_two_comp_to_hex(number, bits = 16):
+#         if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+#             val = val - (1 << bits)        # compute negative value
+#     
+#             #need to now convert to hex
+#         return val           
+   
